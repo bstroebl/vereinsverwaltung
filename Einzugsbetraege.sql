3ef434b579d4157bd629406dea7eee353f97c04c
Bernhard Ströbl <b.stroebl@stroweb.de>
2014-05-03 12:03:45 +0200

    Füge SQL-Abfragen zu Bankeinzügen hinzu

---
+++ Einzugsbetraege.sql
@@ -0,0 +1,9 @@
+﻿--select sum(beitrag)::float from (
+select vorname, mitgliedsname, CASE eintrittsdatum > '2013-06-01'::date WHEN true then 
+COALESCE(individueller_beitrag,12) / 2::float ELSE COALESCE(individueller_beitrag,12) END as beitrag, eintrittsdatum 
+from mitglied m
+where beitragsgruppe_id = 1 
+and eintrittsdatum <= '2013-12-31'::date 
+and zahlungsart_id = 2
+order by mitgliedsname, vorname 
+--) foo
---
+++ eingezogene_zahlung.sql
@@ -0,0 +1,9 @@
+﻿INSERT INTO zahlung(mitglied_id,betrag,zahldatum,hinweis_id)
+select m.id as mitglied_id, CASE eintrittsdatum > '2013-06-01'::date WHEN true then 
+COALESCE(individueller_beitrag,12) / 2::float ELSE COALESCE(individueller_beitrag,12) END as betrag, 
+'2013-12-13'::date as zahldatum, 2 as hinweis_id
+from mitglied m
+where beitragsgruppe_id = 1 
+and eintrittsdatum <= '2013-12-31'::date 
+and zahlungsart_id = 2
+and id != 32 --Martin Gräf		
